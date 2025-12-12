8d0d6762be8b94990eaf83f9f46740b9c3527e95
Pushkar Dubey <pushkardubey1407@gmail.com>
2025-12-12 14:37:35 +0530

    Remove auth middleware from getAllUsers route
    Removed authentication middleware from getAllUsers route.

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -19,7 +19,7 @@ const auth = require("../Middleware/auth");
 
 router.post("/user/register", register);
 router.post("/user/login", login);
-router.get("/user", auth, getAllUsers);
+router.get("/user", getAllUsers);
 router.get("/employeeget/:id", auth, getUserById);
 router.put("/employeeget/:id", auth, updateUser);
 router.delete("/employeedelete/:id", auth, deleteUser);
