b6f5a521b8d4464d429581c9fd22b1627fb37279
Pushkar Dubey <pushkardubey1407@gmail.com>
2026-02-17 01:30:21 +0530

    Fix import case for LeaveTypeController

--- Router/Leave/LeaveTypeRouter.js
+++ Router/Leave/LeaveTypeRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../Middleware/auth");
 const attachCompanyContext = require("../../Middleware/companyMiddleware");
-const { getLeaveTypes, createLeaveType, deleteLeaveType,updateLeaveType } = require("../../Controllers/Leave/leaveTypeController");
+const { getLeaveTypes, createLeaveType, deleteLeaveType,updateLeaveType } = require("../../Controllers/Leave/LeaveTypeController");
 
 router.get("/", auth, attachCompanyContext, getLeaveTypes);
 router.post("/", auth, attachCompanyContext, createLeaveType);
