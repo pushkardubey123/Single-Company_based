304e311b69cf2c95e44daba610d5e107e19c4396
Pushkar Dubey <pushkardubey1407@gmail.com>
2026-02-27 13:33:44 +0530

    Update email transport configuration for Gmail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -11,8 +11,8 @@ const sendEmail = async (
     const transporter = nodemailer.createTransport({
       service: "gmail", // Seedha Gmail service use karein
       host: "smtp.gmail.com",
-      port: 443, // 465 port Render par safely kaam karta hai
-      secure: false, // Port 465 ke liye ye true hona zaroori hai
+      port: 465,// 465 port Render par safely kaam karta hai
+      secure: true, // Port 465 ke liye ye true hona zaroori hai
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS, // Dhyan rahe ye 16-digit App Password ho
